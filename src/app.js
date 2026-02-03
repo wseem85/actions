0b00cfbbf955cf7ca9a6c832afc320ee8c0c625e
wseem85 <engwseem2@gmail.com>
2026-02-04 00:24:33 +0300

    Change the app

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ function greet(name) {
   return `Hello, ${name}`;
 }
 module.exports = { greet }; // just to test if the function is exportable
-if (require.main === module) {
-  // if yes call it
-  console.log(greet('World'));
-}
+// if (require.main === module) {
+//   // if yes call it
+//   console.log(greet('World'));
+// }
